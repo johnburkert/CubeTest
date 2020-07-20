b1ecba95565dd8f691906177ed596622785bb192
John Burkert <bj.59191@konamigaming.com>
2020-07-20 09:46:33 -0700

    added cube rotated by script for visual reference

---
+++ Assets/Scripts/RotateScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateScript : MonoBehaviour
+{
+    public float speed;
+    
+    private void Start()
+    {
+        StartCoroutine(RotateRoutine());
+    }
+    
+    private IEnumerator RotateRoutine()
+    {
+        var rotation = 0f;
+        
+        while (true)
+        {
+            yield return null;
+
+            rotation += speed * Time.deltaTime;
+
+            if (rotation > 360f)
+                rotation -= 360f;
+            
+            transform.rotation = Quaternion.Euler(0f, rotation, 0f);
+        }
+    }
+}
